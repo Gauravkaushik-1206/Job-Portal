f399168e756791dce87b38b57ee4159bb1f63018
gaurav <gaurav@gauravs-MacBook-Air-2.local>
2025-03-05 08:25:23 +0530

    backend deploy

--- job-portal-frontend/src/utils/constant.js
+++ job-portal-frontend/src/utils/constant.js
@@ -1,9 +1,9 @@
-export const User_API_End_Point = "http://localhost:8000/api/v1/user";
-export const Job_API_End_Point = "http://localhost:8000/api/v1/user/job";
-export const Application_API_End_Point = "http://localhost:8000/api/v1/user/application";
-export const Company_API_End_Point = "http://localhost:8000/api/v1/user/company";
+export const User_API_End_Point = "https://job-portal-s8xr.onrender.com/api/v1/user";
+export const Job_API_End_Point = "https://job-portal-s8xr.onrender.com/api/v1/user/job";
+export const Application_API_End_Point = "https://job-portal-s8xr.onrender.com/api/v1/user/application";
+export const Company_API_End_Point = "https://job-portal-s8xr.onrender.com/api/v1/user/company";
 
-export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
-export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/user/company";
+export const USER_API_END_POINT="https://job-portal-s8xr.onrender.com/api/v1/user";
+export const JOB_API_END_POINT="https://job-portal-s8xr.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT="https://job-portal-s8xr.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT="https://job-portal-s8xr.onrender.com/api/v1/user/company";
